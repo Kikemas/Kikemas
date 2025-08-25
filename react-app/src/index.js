3c92ed9692c4ba510c01fe4ef23275529280f666
Enrique Rodríguez Díaz <quiquelado@gmail.com>
2025-08-25 12:48:53 +0200

    changes index.js

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import { createRoot } from 'react-dom/client';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = createRoot(document.getElementById("root"));
+const root = createRoot(document.getElementById('root'));
 root.render(<App />);
