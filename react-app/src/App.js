e4248fe45afa643223de8624d907ca02295387dc
Enrique Rodríguez Díaz <quiquelado@gmail.com>
2025-08-25 12:36:05 +0200

    remove test for Button

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import './Button';
+import Button from './Button';
 
 function App() {
   return (
--- react-app/src/Button.test.js
+++
@@ -1,6 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Button from './Button';
-
-test('renders learn react link', () => {
-  render(<Button />);
-});
